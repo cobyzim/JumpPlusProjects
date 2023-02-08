5753ea4911d7db83b2a23baa74979c9d40af2074
cobyzim <cobyzimmerman.home@gmail.com>
2023-02-08 10:32:43 -0700

    Got login working and started on login options

--- Project1CoreJava/project1-core-java/src/main/java/com/dollarsbank/controller/DollarsBankController.java
+++ Project1CoreJava/project1-core-java/src/main/java/com/dollarsbank/controller/DollarsBankController.java
@@ -13,6 +13,7 @@ import com.dollarsbank.model.Account;
 public class DollarsBankController {
     static Scanner scanner = new Scanner(System.in);
     static boolean isValidChoice = false;
+    static boolean isValidLoginChoice = false;
 
     static Map<Account, List<Float>> accountTransactionMap = new HashMap<>();
     static List<Float> transactionList = new ArrayList<>();
@@ -32,7 +33,7 @@ public class DollarsBankController {
                                                                          // hashmap
                 }
                 if (userChoice == 2) {
-                    // LOGIN
+                    login();
                 }
                 if (userChoice == 3) {
                     System.exit(0);
@@ -59,7 +60,7 @@ public class DollarsBankController {
         return userChoice;
     }
 
-    public static Account createAccount() {
+    public static Account createAccount() { // TODO: Need validation here
         scanner.nextLine(); // Clear buffer
         ConsolePrinterUtility.createAccountPrinter();
         System.out.println("Customer Name: ");
@@ -81,11 +82,93 @@ public class DollarsBankController {
 
         System.out.println("Initial Deposit Amount: ");
         float initialDeposit = scanner.nextFloat();
-        scanner.nextLine();
+        scanner.nextLine();  //TODO: Probaly can get rid of this
 
         Account account = new Account(custName, custAddress, custPhoneNumber, custId, password, initialDeposit);
 
         return account;
+    }
+
+    public static void login() {  //TODO: test this
+        scanner.nextLine();
+        boolean accountFound = false;
+
+        do {
+            ConsolePrinterUtility.loginPrinter();
+
+            System.out.println("User Id: ");
+            int loginId = scanner.nextInt(); // TODO: need validation here
+            scanner.nextLine();
+
+            System.out.println("Password: ");
+            String loginPassword = scanner.nextLine();
+
+            for (Account account : accountTransactionMap.keySet()) {
+                int userId = account.getCustUserId();
+                String userPwd = account.getCustPassword();
+
+                if (userId == loginId && userPwd.equals(loginPassword)) {
+                    accountFound = true;
+                    break;
+                }
+            }
+
+            if (accountFound) {
+                loginOptions();
+
+            } else {
+                System.out.println("Invalid Credentials. Try Again!");
+            }
+        } while (!accountFound);
+    }
+
+    public static void loginOptions() {
+        do {
+            ConsolePrinterUtility.loginWelcomePrinter();
+
+            int userChoice = 0;
+            do {
+                userChoice = getUserLoginChoice();
+                switch (userChoice) {
+                    case 1:
+                        // Make Deposit
+                        break;
+                    case 2:
+                        // Make Withdrawal
+                        break;
+                    case 3:
+                        // Transfer Funds
+                        break;
+                    case 4:
+                        // View 5 Recent Transactions
+                        break;
+                    case 5:
+                        // Display Customer Information
+                        break;
+                    case 6:
+                        welcome();
+                    default:
+                        System.out.println("Invalid Choice!");
+                }
+
+            } while(!isValidLoginChoice);
+        } while(true);
+    }
+
+    private static int getUserLoginChoice() {
+        int userChoice = 0;
+        try {
+            userChoice = scanner.nextInt();
+            if (userChoice == 1 || userChoice == 2 || userChoice == 3 || userChoice == 4 || userChoice == 5 || userChoice == 6) {
+                isValidChoice = true;
+            } else {
+                System.out.println("Please Enter a Valid Choice (1, 2, or 3): ");
+            }
+        } catch (InputMismatchException e) {
+            System.out.println("Please Enter a Valid Numeric Choice (1, 2, or 3): ");
+        }
+
+        return userChoice;
     }
 
 }
--- Project1CoreJava/project1-core-java/src/main/java/com/dollarsbank/utility/ConsolePrinterUtility.java
+++ Project1CoreJava/project1-core-java/src/main/java/com/dollarsbank/utility/ConsolePrinterUtility.java
@@ -17,5 +17,24 @@ public class ConsolePrinterUtility {
         System.out.println("| Enter Details For New Account |");
         System.out.println("+-------------------------------+");
     }
+
+    public static void loginPrinter() {
+        System.out.println("+---------------------+");
+        System.out.println("| Enter Login Details |");
+        System.out.println("+---------------------+");
+    }
     
+    public static void loginWelcomePrinter() {
+        System.out.println("+---------------------+");
+        System.out.println("| WELCOME Customer!!! |");
+        System.out.println("+---------------------+");
+        System.out.println("1. Deposit Amount");
+        System.out.println("2. Withdraw Amount");
+        System.out.println("3. Funds Transfer");
+        System.out.println("4. View 5 Recent Transactions");
+        System.out.println("5. Display Customer Information");
+        System.out.println("6. Sign Out\n");
+        System.out.println("Enter  Choice (1, 2, 3, 4, 5, or 6): ");
+    }
+
 }
