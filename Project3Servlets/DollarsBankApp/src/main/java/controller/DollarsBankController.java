ee8fce6274683bfcab029888bea4f7ab291ce8ee
cobyzim <cobyzimmerman.home@gmail.com>
2023-02-22 09:40:11 -0700

    Project 3 Initialization

---
+++ Project3Servlets/DollarsBankApp/src/main/java/controller/DollarsBankController.java
@@ -0,0 +1,22 @@
+package controller;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import model.Account;
+
+public class DollarsBankController {
+	static boolean isValidChoice = false;
+	static boolean isValidLoginChoice = false;
+	
+	public static int currentAccountId = 0;
+	
+	List<Float> transListForHardAccount = new ArrayList<>();
+	
+	
+	//Account hardAccount = new Account("Buddy Guy", "123 Street", 1234567890, 1, "P@$$w0rd", (float)5000.0, (float)5000.0, 
+	//		5000, "5000.00");
+	
+	
+
+}
---
+++ Project3Servlets/DollarsBankApp/src/main/java/model/Account.java
@@ -0,0 +1,106 @@
+package model;
+
+import java.util.List;
+
+public class Account implements java.io.Serializable {
+
+	private static final long serialVersionUID = 1L;
+	
+	private String custName;
+	private String custAddress;
+	private int custPhone;
+	private int custUserId;
+	private String custPassword;
+	private float custInitialDeposit;
+	private float custBalance;
+	private List<Float> transactions;
+	private List<String> transactionTypes;
+	
+	public Account(String custName, String custAddress, int custPhone, int custUserId, String custPassword,
+			float custInitialDeposit, float custBalance, List<Float> transactions, List<String> transactionTypes) {
+		super();
+		this.custName = custName;
+		this.custAddress = custAddress;
+		this.custPhone = custPhone;
+		this.custUserId = custUserId;
+		this.custPassword = custPassword;
+		this.custInitialDeposit = custInitialDeposit;
+		this.custBalance = custBalance;
+		this.transactions = transactions;
+		this.transactionTypes = transactionTypes;
+	}
+
+	public String getCustName() {
+		return custName;
+	}
+
+	public void setCustName(String custName) {
+		this.custName = custName;
+	}
+
+	public String getCustAddress() {
+		return custAddress;
+	}
+
+	public void setCustAddress(String custAddress) {
+		this.custAddress = custAddress;
+	}
+
+	public int getCustPhone() {
+		return custPhone;
+	}
+
+	public void setCustPhone(int custPhone) {
+		this.custPhone = custPhone;
+	}
+
+	public int getCustUserId() {
+		return custUserId;
+	}
+
+	public void setCustUserId(int custUserId) {
+		this.custUserId = custUserId;
+	}
+
+	public String getCustPassword() {
+		return custPassword;
+	}
+
+	public void setCustPassword(String custPassword) {
+		this.custPassword = custPassword;
+	}
+
+	public float getCustInitialDeposit() {
+		return custInitialDeposit;
+	}
+
+	public void setCustInitialDeposit(float custInitialDeposit) {
+		this.custInitialDeposit = custInitialDeposit;
+	}
+
+	public float getCustBalance() {
+		return custBalance;
+	}
+
+	public void setCustBalance(float custBalance) {
+		this.custBalance = custBalance;
+	}
+
+	public List<Float> getTransactions() {
+		return transactions;
+	}
+
+	public void setTransactions(List<Float> transactions) {
+		this.transactions = transactions;
+	}
+
+	public List<String> getTransactionTypes() {
+		return transactionTypes;
+	}
+
+	public void setTransactionTypes(List<String> transactionTypes) {
+		this.transactionTypes = transactionTypes;
+	}
+	
+
+}
