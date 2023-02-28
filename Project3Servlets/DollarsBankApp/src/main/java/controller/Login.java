20e97235485d54371cf1dbe0cb0109262ee76485
cobyzim <cobyzimmerman.home@gmail.com>
2023-02-28 16:23:23 -0700

    Working validation for login

--- Project3Servlets/DollarsBankApp/src/main/java/controller/Login.java
+++ Project3Servlets/DollarsBankApp/src/main/java/controller/Login.java
@@ -1,6 +1,11 @@
 package controller;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
@@ -17,38 +22,45 @@ import model.AccountDAOImpl;
 @WebServlet("/login")
 public class Login extends HttpServlet {
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public Login() {
-        super();
-    }
 
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#HttpServlet()
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	public Login() {
+		super();
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
 		AccountDAO accountDAO = new AccountDAOImpl();
-		
-		int userId = Integer.parseInt(request.getParameter("id"));
-		String password = request.getParameter("password");
-		
-		Account account = accountDAO.getAccount(userId, password);
-		
-		if (account != null && account.getCustName() != null) {
-			request.getRequestDispatcher("welcome.jsp").forward(request, response);
-		}
-		else {
+		List<String> errList = new ArrayList<>();
+
+		try {
+
+			int userId = Integer.parseInt(request.getParameter("id"));
+			String password = request.getParameter("password");
+
+			Account account = accountDAO.getAccount(userId, password);
+
+			if (account != null && account.getCustName() != null) {
+				request.getRequestDispatcher("welcome.jsp").forward(request, response);
+			} else {
+				errList.add("Invalid Credentials! Try Again or Create and Account");
+				request.setAttribute("errList", errList);
+				
+				request.getRequestDispatcher("login.jsp").forward(request, response);
+			}
+		} catch (NumberFormatException e) {
+			errList.add("Please Make Sure to Input Your Id and Password");
+			request.setAttribute("errList", errList);
+			
 			request.getRequestDispatcher("login.jsp").forward(request, response);
 		}
-		
-		
-		
-		
-		
-		
-		
+
 	}
 
 }
--- Project3Servlets/DollarsBankApp/src/main/webapp/login.jsp
+++ Project3Servlets/DollarsBankApp/src/main/webapp/login.jsp
@@ -1,5 +1,7 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
     pageEncoding="UTF-8"%>
+<%@ page import="java.util.List" %> 
+<%@ page import="java.util.ArrayList" %>   
 <!DOCTYPE html>
 <html>
 <head>
@@ -7,6 +9,19 @@
 <title>Login</title>
 </head>
 <body>
+	<%
+		List<String> errList = (ArrayList<String>) request.getAttribute("errList");
+		if (errList != null) {
+			for (String error : errList) {
+				%>
+				<script>
+					alert("<%=error%>");
+				</script>
+				<%
+			}
+		}
+	%>
+
 	<div>
 		<h1>Welcome to DollarsBank!</h1>
 	</div>
